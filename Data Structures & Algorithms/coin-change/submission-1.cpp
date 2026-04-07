7e78aac207110bcc5d4655c907564d5bb9a6b571
Romain Desarzens <hib.pub@protonmail.com>
2026-04-07 17:56:06 +0200

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        std::deque<uint64_t> q;
+        std::vector<uint64_t> n_coins(amount + 1, -1);
+
+        n_coins[0] = 0;
+        q.push_back(0);
+        while(!q.empty())
+        {
+            auto val = q.front();
+            q.pop_front();
+
+            if (val == amount)
+            {
+                return n_coins[amount];
+            }
+
+            for (auto& coin : coins)
+            {
+                if ((val + coin) <= amount && n_coins[val + coin] == -1)
+                {
+                    n_coins[val + coin] = n_coins[val] + 1;
+                    q.push_back(val + coin);
+                }
+            }
+        }
+
+        return -1;
+    }
+};
